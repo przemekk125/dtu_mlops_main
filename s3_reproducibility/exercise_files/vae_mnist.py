4caf322ff74c88f4596033785e85f10c07ec45fb
przemekk125 <przemek.kaleta@vp.pl>
2026-01-09 13:57:39 +0100

    Completed Hydra module

--- s3_reproducibility/exercise_files/vae_mnist.py
+++ s3_reproducibility/exercise_files/vae_mnist.py
@@ -5,6 +5,7 @@ A simple implementation of Gaussian MLP Encoder and Decoder trained on MNIST
 
 import os
 
+import hydra
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
@@ -18,77 +19,90 @@ from torchvision.utils import save_image
 dataset_path = "~/datasets"
 cuda = torch.cuda.is_available()
 DEVICE = torch.device("cuda" if cuda else "cpu")
-batch_size = 100
-x_dim = 784
-hidden_dim = 400
 
-# Data loading
-mnist_transform = transforms.Compose([transforms.ToTensor()])
+# + latent_dim
+# + seed
+# + learning rate
+# + num_epochs
 
-train_dataset = MNIST(dataset_path, transform=mnist_transform, train=True, download=True)
-test_dataset = MNIST(dataset_path, transform=mnist_transform, train=False, download=True)
+@hydra.main(config_name="config.yaml")
+def train(cfg):
+    torch.manual_seed(cfg.hyperparameters.seed)
+    # Data loading
+    mnist_transform = transforms.Compose([transforms.ToTensor()])
 
-train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+    train_dataset = MNIST(dataset_path, transform=mnist_transform, train=True, download=True)
 
-encoder = Encoder(input_dim=x_dim, hidden_dim=hidden_dim, latent_dim=20)
-decoder = Decoder(latent_dim=20, hidden_dim=hidden_dim, output_dim=x_dim)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=cfg.hyperparameters.batch_size,
+                              shuffle=True)
 
-model = Model(encoder=encoder, decoder=decoder).to(DEVICE)
+    encoder = Encoder(input_dim=cfg.hyperparameters.x_dim, hidden_dim=cfg.hyperparameters.hidden_dim,
+                      latent_dim=cfg.hyperparameters.latent_dim)
+    decoder = Decoder(latent_dim=cfg.hyperparameters.latent_dim, hidden_dim=cfg.hyperparameters.hidden_dim,
+                      output_dim=cfg.hyperparameters.x_dim)
 
-
-def loss_function(x, x_hat, mean, log_var):
-    """Elbo loss function."""
-    reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction="sum")
-    kld = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
-    return reproduction_loss + kld
-
-
-optimizer = Adam(model.parameters(), lr=1e-3)
+    model = Model(encoder=encoder, decoder=decoder).to(DEVICE)
 
 
-print("Start training VAE...")
-model.train()
-for epoch in range(20):
-    overall_loss = 0
-    for batch_idx, (x, _) in enumerate(train_loader):
-        if batch_idx % 100 == 0:
-            print(batch_idx)
-        x = x.view(batch_size, x_dim)
-        x = x.to(DEVICE)
+    def loss_function(x, x_hat, mean, log_var):
+        """Elbo loss function."""
+        reproduction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction="sum")
+        kld = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
+        return reproduction_loss + kld
 
-        optimizer.zero_grad()
 
-        x_hat, mean, log_var = model(x)
-        loss = loss_function(x, x_hat, mean, log_var)
+    optimizer = Adam(model.parameters(), lr=cfg.hyperparameters.learning_rate)
 
-        overall_loss += loss.item()
 
-        loss.backward()
-        optimizer.step()
-    print(f"Epoch {epoch + 1} complete!,  Average Loss: {overall_loss / (batch_idx * batch_size)}")
-print("Finish!!")
+    print("Start training VAE...")
+    model.train()
+    for epoch in range(cfg.hyperparameters.num_epochs):
+        overall_loss = 0
+        for batch_idx, (x, _) in enumerate(train_loader):
+            if batch_idx % 100 == 0:
+                print(batch_idx)
+            x = x.view(cfg.hyperparameters.batch_size, cfg.hyperparameters.x_dim)
+            x = x.to(DEVICE)
 
-# save weights
-torch.save(model, f"{os.getcwd()}/trained_model.pt")
+            optimizer.zero_grad()
 
-# Generate reconstructions
-model.eval()
-with torch.no_grad():
-    for batch_idx, (x, _) in enumerate(test_loader):
-        if batch_idx % 100 == 0:
-            print(batch_idx)
-        x = x.view(batch_size, x_dim)
-        x = x.to(DEVICE)
-        x_hat, _, _ = model(x)
-        break
+            x_hat, mean, log_var = model(x)
+            loss = loss_function(x, x_hat, mean, log_var)
 
-save_image(x.view(batch_size, 1, 28, 28), "orig_data.png")
-save_image(x_hat.view(batch_size, 1, 28, 28), "reconstructions.png")
+            overall_loss += loss.item()
 
-# Generate samples
-with torch.no_grad():
-    noise = torch.randn(batch_size, 20).to(DEVICE)
-    generated_images = decoder(noise)
+            loss.backward()
+            optimizer.step()
+        print(f"Epoch {epoch + 1} complete!,  Average Loss: {overall_loss / (batch_idx * cfg.hyperparameters.batch_size)}")
+    print("Finish!!")
 
-save_image(generated_images.view(batch_size, 1, 28, 28), "generated_sample.png")
+    # save weights
+    torch.save(model, f"{os.getcwd()}/trained_model.pt")
+
+    mnist_transform = transforms.Compose([transforms.ToTensor()])
+    test_dataset = MNIST(dataset_path, transform=mnist_transform, train=False, download=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=cfg.hyperparameters.batch_size, shuffle=False)
+
+    # Generate reconstructions
+    model.eval()
+    with torch.no_grad():
+        for batch_idx, (x, _) in enumerate(test_loader):
+            if batch_idx % 100 == 0:
+                print(batch_idx)
+            x = x.view(cfg.hyperparameters.batch_size, cfg.hyperparameters.x_dim)
+            x = x.to(DEVICE)
+            x_hat, _, _ = model(x)
+            break
+
+    save_image(x.view(cfg.hyperparameters.batch_size, 1, 28, 28), "orig_data.png")
+    save_image(x_hat.view(cfg.hyperparameters.batch_size, 1, 28, 28), "reconstructions.png")
+
+    # Generate samples
+    with torch.no_grad():
+        noise = torch.randn(cfg.hyperparameters.batch_size, cfg.hyperparameters.latent_dim).to(DEVICE)
+        generated_images = model.decoder(noise)
+
+    save_image(generated_images.view(cfg.hyperparameters.batch_size, 1, 28, 28), "generated_sample.png")
+
+if __name__ == "__main__":
+    train()
